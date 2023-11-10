7474850c3ece93e0a5522b52dee539efaa20f59f
Ian <ianlemee@hotmail.com>
2023-11-10 15:30:45 -0300

    fixando script

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -83,7 +83,7 @@ const handleLogin = (event) => {
     login.style.display = "none"
     chat.style.display = "flex"
 
-    websocket = new WebSocket("ws://chat-backend-taxq.onrender.com")
+    websocket = new WebSocket("wss://chat-backend-taxq.onrender.com");
     websocket.onmessage = processMessage
 }
 
